27421ae5d060c7d302ace8a0bf1a78b9d2b30797
christian.dehek <christian.dehek@gmail.com>
2020-10-11 09:43:15 -0600

    setup initial express/setup get requests '

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3050;
+const { notes } = require('./Develop/db/db.json');
+// const mainDir = path.join(__dirname, "/public");
+
+app.use(express.static('./Develop/public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming data
+app.use(express.json());
+
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+
+app.listen(3050, () => {
+    console.log(`API server now on port 3050!`);
+    // console.log(notes);
+});
